4086dccf3c429814de41d73128b65c3b6b1b687c
LuisArturo21 <91095903+LuisArturo21@users.noreply.github.com>
2025-10-23 09:46:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import tkinter as tk
+from presentation.game_ui import GameUI
 
+def main():
+    root = tk.Tk()
+    root.title("Piano Tiles")
+    root.resizable(False, False)
+    
+    game_ui = GameUI(root)
+    
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()#main.py
