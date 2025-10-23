26deb87537ed97fdcc084d3fceaff053e91ddf8d
lmfontes <luis.fontes.cardona@intel.com>
2025-10-23 09:39:56 -0600

    Call modules into main

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import tkinter as tk
-from game import Game
+from controllers.game import GameController
+from views.views import GameView
+
 
 def main():
     root = tk.Tk()
-    root.title("Piano Tiles")
+    root.title("Piano Tiles Game")
     root.resizable(False, False)
-    
-    game = Game(root)
-    
+
+    root.update_idletasks()
+    width = root.winfo_reqwidth()
+    height = root.winfo_reqheight()
+    pos_x = (root.winfo_screenwidth() // 2) - (width // 2)
+    pos_y = (root.winfo_screenheight() // 2) - (height // 2)
+    root.geometry(f"+{pos_x}+{pos_y}")
+
+    view = GameView(root)
+
+    controller = GameController(view)
+
     root.mainloop()
 
+
 if __name__ == "__main__":
-    main()#main.py
+    main()
